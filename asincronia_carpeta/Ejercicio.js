2896dd43e39a26c3517f96191802193eef08bb42
KevinPaez28 <Kevinandreypaezgarces@gmail.com>
2025-02-28 06:42:13 -0500

    ejercicio1

---
+++ asincronia_carpeta/Ejercicio.js
@@ -0,0 +1,32 @@
+function mostrarmensaje(desde, hasta) {
+  ;
+  let intervalo = setInterval(() => { //set interval se encarga de ejecutar la funcion cada segundo
+    console.log(`parte 1: ${desde}`) //imprime el numero actual
+
+    if (desde == hasta) { //si el numero desde sea igual al numero hasta y se cumpla la funcion detenga el setinterval
+      clearInterval(intervalo);
+    }
+    desde++; //aumentamos el numero para la siguiente interacion
+  },1000)
+}
+
+function Cuentanumerica(desde1, hasta1) { 
+  function mostrarnumero() {
+    console.log(`Números 2: ${desde1}`); 
+    
+    if (desde1 < hasta1) {  // Si se cumple la condicion llama la funcion de nuevo cada 1 segundo
+      desde1++;  
+      setTimeout(mostrarnumero, 1000);  
+    }
+  }
+
+  mostrarnumero(); // iniciamos ejecucion
+}
+
+let desde  = parseInt(prompt("Ingrese hasta donde quiere que inicie la cuenta de los numeros")) //Pedimos que el usuario ingrese los datos 
+let hasta = parseInt(prompt("Ingrese hasta donde quiere que lleguen la cuenta de los numeros"))
+mostrarmensaje(desde, hasta) //llamamos a la funcion
+
+let desde1 = parseInt(prompt("Ingrese hasta donde quiere que inicie la cuenta de los numeros(segundo metodo)")) //Pedimos que el usuario ingrese los datos 
+let hasta1 = parseInt(prompt("Ingrese hasta donde quiere que lleguen la cuenta de los numeros(segundo metodo)"))
+Cuentanumerica(desde1,hasta1)
