2e8d0a32ce1b734a664bd413b2df466d267ef729
KevinPaez28 <Kevinandreypaezgarces@gmail.com>
2025-03-05 11:14:36 -0500

    adelanto_ejercicios2.0

--- asincronia_js/ejercicio15.js
+++ asincronia_js/ejercicio15.js
@@ -1,3 +1,9 @@
 // 15. Realiza una llamada a un archivo local Muestra en formato json, luego muestre los datos
 // obtenidos en la consola.
 
+fetch("../datos_json/ejercicio15.json").then(function (data){
+  const respuesta = data.json().then(function(dato){
+    console.log(dato)
+  })
+})
+
